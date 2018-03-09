fa68e3bbbb386a37e5304225f7bbafeaee642bc5
t3rmin41 <t33rmin41@gmail.com>
2018-03-09 16:56:59 +0200

    Minimal SpringBoot

--- src/main/java/com/domain/app/App.java
+++ src/main/java/com/domain/app/App.java
@@ -1,5 +1,23 @@
 package com.domain.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import com.domain.config.AppConfig;
+
+@SpringBootApplication
+@Import({AppConfig.class})
 public class App {
 
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+  
+  public static void main(String[] args) {
+    SpringApplication springApplication = new SpringApplication(App.class);
+    ApplicationContext context = springApplication.run(args);
+    logger.warn("Context : " + context.getId());
+  }
 }
--- src/main/java/com/domain/config/AppConfig.java
+++ src/main/java/com/domain/config/AppConfig.java
@@ -1,5 +1,10 @@
 package com.domain.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {""})
 public class AppConfig {
 
 }
